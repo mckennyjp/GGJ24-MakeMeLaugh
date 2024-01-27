6f9988460c27be5172cd64b37b6357de1b528fa1
mckennyjp <jumbomax792@gmail.com>
2024-01-26 18:47:12 -0600

    Voice Command Script Created

---
+++ Laughing Shooter/Assets/PlayerVoiceCommand.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class PlayerVoiceCommand : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    void Start()
+    {
+        actions.Add("ha", Forward);
+        actions.Add("he", Back);
+        actions.Add("ho", Down);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+    private void Forward()
+    {
+        transform.Translate(1, 0, 0);
+    }
+    private void Back()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+    private void Up()
+    {
+        transform.Translate(0, 1, 0);
+    }
+    private void Down()
+    {
+        transform.Translate(0, -1, 0);
+    }
+}
